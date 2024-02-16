a3e1e2f2e5fbe3d692ba2a638e8d4bfb6d90d907
Sirinemn <sirinemn@outlook.fr>
2024-02-16 16:05:15 +0100

    mappers

--- back/target/generated-sources/annotations/com/openclassrooms/mddapi/mapper/ArticleMapperImpl.java
+++ back/target/generated-sources/annotations/com/openclassrooms/mddapi/mapper/ArticleMapperImpl.java
@@ -16,7 +16,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-02-16T14:45:44+0100",
+    date = "2024-02-16T15:57:05+0100",
     comments = "version: 1.5.1.Final, compiler: Eclipse JDT (IDE) 3.35.0.v20230814-2020, environment: Java 17.0.8.1 (Eclipse Adoptium)"
 )
 @Component
